f2747168f5e0aaf1f3be7a123ec12c8e2f754b6f
Patrick Steiner <patrick@steiner-buchholz.de>
2016-01-29 13:08:46 +0100

    solves and closes #5

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "-Building deloyables"
+
+export BUILD_LOGFILE="../build.log"
+
+echo "-- iot_datacenter_receiver"
+pushd . > /dev/null
+cd receiver
+mvn clean install >>  $BUILD_LOGFILE
+popd > /dev/null
+
+echo "-- iot_datacenter_monitor"
+pushd . > /dev/null
+cd monitor
+mvn clean install >>  $BUILD_LOGFILE
+popd > /dev/null
+
+echo "-- Rules Server deployable"
+pushd . > /dev/null
+cd rules
+mvn clean install >>  $BUILD_LOGFILE
+popd > /dev/null
+
+echo "-Building all images for Fuse IoT Demo"
+echo "-- Building Base Image"
+pushd .
+cd Base
+docker build --rm -t psteiner/base .
+popd
+
+echo "-- Building Fuse Image"
+pushd .
+cd Fuse
+docker build --rm -t psteiner/fuse .
+popd
+
+echo "-- Building docker-compose based images"
+docker-compose build
