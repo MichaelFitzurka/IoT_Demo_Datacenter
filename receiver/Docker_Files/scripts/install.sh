6f856e684b8283b552da83bb7f99c26e720fee77
Patrick Steiner <Patrick@Steiner-Buchholz.de>
2017-02-16 16:43:06 +0100

    updated JBoss Fuse to V6.3

--- receiver/Docker_Files/scripts/install.sh
+++ receiver/Docker_Files/scripts/install.sh
@@ -9,20 +9,16 @@
 echo "Start Fuse and wait for start procedure to end"
 $HOME/$FUSE_LOCATION/bin/start
 
-$HOME/$FUSE_LOCATION/bin/status
-while [ "$?" != "0" ]
+while [ "`$HOME/$FUSE_LOCATION/bin/status`" != "Running ..." ]
 do
-   echo "."
-   sleep 10
-   $HOME/$FUSE_LOCATION/bin/status
+echo 'Fuse not available ... waiting 5 seconds'
+sleep 5
 done
-
-echo "waiting for 5 minutes, until I found a more clever way to check if Fuse is running"
-
-sleep 300
 
 echo "Now let's deploy the bundle and some prereqs"
 echo "commons-dbcp"
-$HOME/$FUSE_LOCATION/bin/client "osgi:install -s wrap:mvn:commons-dbcp/commons-dbcp/1.4"
+$HOME/$FUSE_LOCATION/bin/client -h 127.0.0.1 -r 60 -d 10 "osgi:install -s wrap:mvn:commons-dbcp/commons-dbcp/1.4"
 echo "postgresql"
-$HOME/$FUSE_LOCATION/bin/client "osgi:install -s wrap:mvn:org.postgresql/postgresql/9.3-1102-jdbc41"
+$HOME/$FUSE_LOCATION/bin/client -h 127.0.0.1 -r 60 -d 10 "osgi:install -s wrap:mvn:org.postgresql/postgresql/9.3-1102-jdbc41"
+echo "camel-jetty"
+$HOME/$FUSE_LOCATION/bin/client -h 127.0.0.1 -r 60 -d 10 "features:install camel-jetty"
